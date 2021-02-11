0a3da79d106bc5b9982c68c99f52dd484ab13b90
Abigail M-A <70247056+abimacarmes@users.noreply.github.com>
2021-02-11 15:12:54 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ app.use(function errorHandler(error, req, res, next){
 
 app.use(function validateBearerToken(req,res,next){
     const apiToken=process.env.API_TOKEN
-    const authToken = req.get('Authorization')
+    const authToken = req.get('Authorization').split(" ")[1]
 
     if(!authToken || authToken !== apiToken){
         logger.error(`Unauthorized request to path: ${req.path}`);
